930767e29dd409f050c377d913b4bf50e33dd0ff
AmeyaDaddikar <ameyadaddikar@gmail.com>
2017-11-05 10:04:22 +0530

    Added a small easter egg, (Android : You are now a developer!!!!easter egg) Cleaned up the MainPage fragment

--- app/src/main/java/com/example/coldball/codehunt/MainPage.java
+++ app/src/main/java/com/example/coldball/codehunt/MainPage.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -16,57 +17,46 @@ import android.widget.Button;
  * Activities that contain this fragment must implement the
  * {@link MainPage.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link MainPage#newInstance} factory method to
- * create an instance of this fragment.
+ *
  */
-public class MainPage extends Fragment {
+public class MainPage extends Fragment
+    implements View.OnClickListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    
+    private static int counter = 5;
+    public Button start_button;
 
     private OnFragmentInteractionListener mListener;
-
-    public MainPage() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainPage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MainPage newInstance(String param1, String param2) {
-        MainPage fragment = new MainPage();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_page, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_main_page, container, false);
+        start_button = (Button) view.findViewById(R.id.start_button);
+        start_button.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(counter > 0)
+            Toast.makeText(getContext(),
+                    "You are " + String.valueOf(counter--) + " steps away from starting codehunt",
+                    Toast.LENGTH_SHORT).show();
+        else{
+            Toast.makeText(getContext(), "LET THE GAMES BEGIN!!!!",
+                    Toast.LENGTH_SHORT).show();
+            //SWITCH FRAGMENT IN MAIN ACTIVITY HERE
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -107,4 +97,5 @@ public class MainPage extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
 }
